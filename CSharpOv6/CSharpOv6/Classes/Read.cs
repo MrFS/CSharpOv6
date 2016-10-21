c44477a617b97ec101aa3008fa055c0c1c42483b
Joachim Flatberg Stamnes <joachifs@student.hist.no>
2016-10-21 21:18:27 +0200

    done

---
+++ CSharpOv6/CSharpOv6/Classes/Read.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace CSharpOv6.Classes
+{
+    public class Read
+    {
+        //Key = Ord i Håvamål - Value = Hvilken strofe ordet eksisterer i
+        private Dictionary<string, string> _OrdPaStrofe = new Dictionary<string, string>();
+
+        public Dictionary<string, int> File(string filename)
+        {
+
+            //Key = Ord i Håvamål - Value = Antall ganger ordet gjentar seg i Håvamål
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            
+            string strofe = "";
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    string line = sr.ReadLine();
+
+                    if (line.Contains("Strofe"))
+                    {
+                        strofe = KunTall(line);
+                    }
+
+                    string[] words = line.Split(' ');
+
+                    foreach (string word in words)
+                    {
+
+                        string lele = RemoveSpecialCharacters(word);
+                        lele = lele.ToLower();
+                        
+                        if (strofe != string.Empty)
+                        {
+                            if (_OrdPaStrofe.ContainsKey(lele))
+                            {
+                                if (!(_OrdPaStrofe[lele].Contains(strofe)))
+                                {
+                                    _OrdPaStrofe[lele] += strofe + ", ";
+                                }
+                                else
+                                {
+                                    _OrdPaStrofe[lele] = _OrdPaStrofe[lele].Remove(_OrdPaStrofe[lele].Length - 2);
+                                    _OrdPaStrofe[lele] += "*, ";
+                                }
+                            }
+                            else
+                            {
+                                _OrdPaStrofe[lele] = strofe + ", ";
+                            }
+                        }
+                        
+                        if (lele != string.Empty && lele != "strofe")
+                        {
+                            if (dictionary.ContainsKey(lele))
+                            {
+                                // If in dic we ++
+                                dictionary[lele]++;
+                            }
+                            else
+                            {
+                                // If not in dic we add
+                                dictionary[lele] = 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            Dictionary<string, int> sortedDict = new Dictionary<string, int>();
+
+            var sorted = dictionary.Keys.ToList();
+            sorted.Sort();
+
+            foreach (var key in sorted)
+            {
+                sortedDict.Add(key, dictionary[key]);
+            }
+
+            return sortedDict;
+        }
+
+        public Dictionary<string, string> OrdPaStrofe
+        {
+            get { return _OrdPaStrofe; }
+        }
+
+        private static string RemoveSpecialCharacters(string str)
+        {
+            return Regex.Replace(str, "[^a-åA-Å]", "", RegexOptions.Compiled);
+        }
+
+        private static string KunTall(string str)
+        {
+            return Regex.Replace(str, "[^0-9]", "", RegexOptions.Compiled);
+        }
+    }
+}
--- CSharpOv6/CSharpOv6/Program.cs
+++ CSharpOv6/CSharpOv6/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Text.RegularExpressions;
+using CSharpOv6.Classes;
 
 namespace CSharpOv6
 {
@@ -13,90 +14,29 @@ namespace CSharpOv6
         static void Main(string[] args)
         {
 
-            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            Read rd = new Read();
 
-            Dictionary<string, int> count = new Dictionary<string, int>();
+            Dictionary<string, int> dictionary = rd.File("havamal.txt");
 
-            SortedDictionary<string, List<int>> srt = new SortedDictionary<string, List<int>>();
+            Dictionary<string, string> Strofe = rd.OrdPaStrofe;
 
-            string strofe = "";
-
-            using (StreamReader sr = new StreamReader("havamal.txt"))
-            {
-                while (sr.Peek() >= 0)
-                {
-                    string line = sr.ReadLine();
-
-                    if (line.Contains("Strofe"))
-                    {
-                        strofe = line;
-                    }
-
-                    string[] words = line.Split(' ');
-                    foreach (string word in words)
-                    {
-
-                        string lele = RemoveSpecialCharacters(word);
-
-                        lele = lele.ToLower();
-
-                        if (lele != string.Empty && lele != "strofe")
-                        {
-                            if (dictionary.ContainsKey(lele))
-                            {
-                                // If in dic we ++
-                                dictionary[lele]++;
-                            }
-                            else
-                            {
-                                // If not in dic we add
-                                dictionary[lele] = 1;
-                            }
-                        }
-                        
-
-
-                        //Console.WriteLine(lele);
-
-                        foreach (Char c in word)
-                        {
-                            // ...
-                        }
-                    }
-                    
-
-                }
-            }
-
-
-            //foreach (var item in dictionary)
-            //{
-            //    Console.WriteLine(item.Key + "\t" + item.Value);
-            //}
-
-            //Console.WriteLine(strofe);
-
-            Dictionary<string, int> sortedDict = (from entry in dictionary
-                              orderby entry.Value descending select entry).ToDictionary(pair => pair.Key, pair => pair.Value);
-
-            //count loop
-            int countt = 1;
+            int count = 1;
             
-            Console.WriteLine();
-            foreach (KeyValuePair<string, int> pair in sortedDict)
+            Console.WriteLine("#\tOrd\t\tAntall\tStrofe");
+            foreach (KeyValuePair<string, int> pair in dictionary)
             {
                 //loop sorted dic from dic
 
                 if (pair.Key.Length > 7)
                 {
-                    Console.WriteLine(countt + "\t" + pair.Key + "\t" + pair.Value);
+                    Console.WriteLine(count + "\t" + pair.Key + "\t" + pair.Value + "\t" + Strofe[pair.Key]);
                 }
                 else
                 {
-                    Console.WriteLine(countt + "\t" + pair.Key + "\t\t" + pair.Value);
+                    Console.WriteLine(count + "\t" + pair.Key + "\t\t" + pair.Value + "\t" + Strofe[pair.Key]);
                 }
                 
-                countt++;
+                count++;
 
                 //if count = 10 brek, dis maks 10 words ut
                 //if (countt > 10)
@@ -104,15 +44,8 @@ namespace CSharpOv6
                 //    break;
                 //}
             }
-
-
+            
             Console.Read();
-        }
-
-
-        public static string RemoveSpecialCharacters(string str)
-        {
-            return Regex.Replace(str, "[^a-åA-Å]$", "", RegexOptions.Compiled);
         }
     }
 }
